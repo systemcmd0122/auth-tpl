b9fb44d992c27c3118a0c1401e0da4f5740f9364
Tisk_01010100 <systemcmd02.address@gmail.com>
2025-07-24 13:45:01 +0900

    Update layout.tsx

--- app/signup/layout.tsx
+++ app/signup/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "アカウント作成",
-  description: "TechDash Proのアカウントを作成して、最先端の技術的ダッシュボードを体験してください。",
+  description: "auth-tplのアカウントを作成",
   robots: {
     index: true,
     follow: true,
